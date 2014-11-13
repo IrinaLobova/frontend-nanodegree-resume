6ec739ce8ff5fa2b85a2feaae543a75f16e80a54
Irina Lobova <lobovais@gmail.com>
2014-11-12 19:09:12 -0500

    completed projects, education and map sections, customized css

--- js/helper.js
+++ js/helper.js
@@ -12,21 +12,20 @@ Cameron Pittman
 These are HTML strings. As part of the course, you'll be using JavaScript functions
 replace the %data% placeholder text you see in them.
 */
-var HTMLheaderName = "<h1 id='name'>%data%</h1>";
-var HTMLheaderRole = "<span>%data%</span><hr/>";
+var HTMLheaderName = "<h1 id='name' class='small-12 large-7 columns'>%data%</h1>";
+var internationalizeButton = "<button class='tiny button large-2 columns'>Internationalize</button>";
+var HTMLheaderRole = "<span id='role' class='small-12 large-9 columns'>%data%</span>";
 
-var HTMLcontactGeneric = "<li class='flex-item'><span class='orange-text'>%contact%</span><span class='white-text'>%data%</span></li>";
-var HTMLmobile = "<li class='flex-item'><span class='orange-text'>mobile</span><span class='white-text'>%data%</span></li>";
-var HTMLemail = "<li class='flex-item'><span class='orange-text'>email</span><span class='white-text'>%data%</span></li>";
-var HTMLtwitter = "<li class='flex-item'><span class='orange-text'>twitter</span><span class='white-text'>%data%</span></li>";
-var HTMLgithub = "<li class='flex-item'><span class='orange-text'>github</span><span class='white-text'>%data%</span></li>";
-var HTMLblog = "<li class='flex-item'><span class='orange-text'>blog</span><span class='white-text'>%data%</span></li>";
-var HTMLlocation = "<li class='flex-item'><span class='orange-text'>location</span><span class='white-text'>%data%</span></li>";
+var HTMLcontactGeneric = "<li class='flex-item'><span class='orange-text'>%contact%</span><span>%data%</span></li>";
+var HTMLmobile = "<li class='flex-item large-3  columns'><span class='orange-text'>mobile</span><span>%data%</span></li>";
+var HTMLemail = "<li class='flex-item large-4 columns'><span class='orange-text'>email</span><span>%data%</span></li>";
+var HTMLgithub = "<li class='flex-item large-2 columns'><span class='orange-text'>github</span><span>%data%</span></li>";
+var HTMLlocation = "<li class='flex-item large-2 columns'><span class='orange-text'>location</span><span>%data%</span></li>";
 
-var HTMLbioPic = "<img src='%data%' class='biopic'>";
-var HTMLWelcomeMsg = "<span class='welcome-message'>%data%</span>";
+var HTMLbioPic = "<img src='%data%' class='biopic large-3 columns show-for-large-only'>";
+var HTMLWelcomeMsg = "<span class='welcome-message small-12 large-9 columns'>%data%</span>";
 
-var HTMLskillsStart = "<h3 class='skillsH3'>Skills at a Glance:</h3><ul id='skills' class='flex-box'></ul>";
+var HTMLskillsStart = "<h3 class='skillsH3 small-12 large-9 columns'>Skills at a Glance:</h3><ul id='skills' class='flex-box small-12 large-7 columns'></ul>";
 var HTMLskills = "<li class='flex-item'><span class='white-text'>%data%</span></li>";
 
 var HTMLworkStart = "<div class='work-entry'></div>";
@@ -34,28 +33,28 @@ var HTMLworkEmployer = "<a href='#'>%data%";
 var HTMLworkTitle = " - %data%</a>";
 var HTMLworkDates = "<div class='date-text'>%data%</div>";
 var HTMLworkLocation = "<div class='location-text'>%data%</div>";
-var HTMLworkDescription = "<p><br>%data%</p>";
+var HTMLworkDescription = "<p>%data%</p>";
 
-var HTMLprojectStart = "<div class='project-entry'></div>";
+var HTMLprojectStart = "<li class='project-entry'></li>";
 var HTMLprojectTitle = "<a href='#'>%data%</a>";
 var HTMLprojectDates = "<div class='date-text'>%data%</div>";
-var HTMLprojectDescription = "<p><br>%data%</p>";
+var HTMLprojectDescription = "<p class='hide-for-small-only'><br>%data%</p>";
 var HTMLprojectImage = "<img src='%data%'>";
 
 var HTMLschoolStart = "<div class='education-entry'></div>";
 var HTMLschoolName = "<a href='#'>%data%";
 var HTMLschoolDegree = " -- %data%</a>";
-var HTMLschoolDates = "<div class='date-text'>%data%</div>";
 var HTMLschoolLocation = "<div class='location-text'>%data%</div>";
-var HTMLschoolMajor = "<em><br>Major: %data%</em>"
+var HTMLschoolDates = "<div class='date-text'>%data%</div>";
+var HTMLschoolMajor = "<em>Major: %data%</em>"
 
+var HTMLonlineStart = "<div class='online-entry'></div>";
 var HTMLonlineClasses = "<h3>Online Classes</h3>";
 var HTMLonlineTitle = "<a href='#'>%data%";
 var HTMLonlineSchool = " - %data%</a>";
 var HTMLonlineDates = "<div class='date-text'>%data%</div>";
-var HTMLonlineURL = "<br><a href='#'>%data%</a>";
+var HTMLonlineURL = "<a href='#'>%data%</a>";
 
-var internationalizeButton = "<button>Internationalize</button>";
 var googleMap = "<div id='map'></div>";
 
 
@@ -68,28 +67,6 @@ $(document).ready(function() {
     $('#name').html(iName);  
   });
 })
-
-
-
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
-*/
-clickLocations = [];
-
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      "x": x,
-      "y": y
-    }
-  );
-  console.log("x location: " + x + "; y location: " + y);
-}
-
-$(document).click(function(loc) {
-  // your code goes here!
-});
-
 
 
 /*
@@ -235,11 +212,11 @@ Uncomment all the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window 
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+//   Make sure the map bounds get updated on page resize
+map.fitBounds(mapBounds);
+ });
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,7 +5,6 @@ var bio = {
 		"mobile": "617-500-82-51",
 		"email": "irina.lobova@me.com",
 		"github": "irinalobova",
-		"twitter": "@irinalobova",
 		"location": "Boston"
 	},
 	"welcomeMessage": "Welcome to my resume page!",
@@ -26,7 +25,7 @@ var education = {
 			"name": "Harvard Extension School",
 			"location": "Cambridge MA",
 			"degree": "BA",
-			"major": "Degital Media",
+			"major": "Digital Media",
 			"dates": 2016,
 			"url": "http://www.extension.harvard.edu/"
 			}
@@ -59,14 +58,14 @@ var work = {
 			"title": "credit inspector",
 			"location": "Moscow",
 			"dates": "2009-2011",
-			"description": "lorem ipsum ..."
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 		},
 		{
 			"employer": "PromsvyazBank",
 			"title": "economist",
 			"location": "Moscow",
 			"dates": "2011-2012",
-			"description": "lorem ipsum ..."
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 		}
 	]
 };
@@ -75,25 +74,26 @@ var projects = {
 		{
 			"title": "RDSY Website",
 			"dates": 2013,
-			"description": "lorem ipsum ...",
-			"images": ["http://example1.com", "http://example2.com", "http://example3.com"] 
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+			"image": "images/rdsy.jpg" 
 		},
 		{
 			"title": "RDSY Website",
 			"dates": 2013,
-			"description": "lorem ipsum ...",
-			"images": ["http://example1.com", "http://example2.com", "http://example3.com"] 
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+			"image": "images/foodapp.jpg" 
 		},
 		{
 			"title": "RDSY Website",
 			"dates": 2013,
-			"description": "lorem ipsum ...",
-			"images": ["http://example1.com", "http://example2.com", "http://example3.com"]
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+			"image": "images/aviasales.jpg"
 		}
 	]
 };
 
-//* * * * * Name and Role * * * * *//
+/* * * * * Name and Role * * * * */
+
 if(bio.role.length > 0) {
 	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 	$("#header").prepend(formattedRole);
@@ -104,7 +104,7 @@ if(bio.name.length > 0) {
 	$("#header").prepend(formattedName);
 }
 
-//* * * * * Contacts in header * * * * *//
+/* * * * * Contacts * * * * */
 
 if(bio.contacts.length > 0) {
 	var formattedContacts = HTMLcontactGeneric.replace("%data%", bio.contacts);
@@ -130,19 +130,13 @@ if(bio.contacts.github.length > 0) {
 	$("#footerContacts").append(formattedGithub);
 }
 
-if(bio.contacts.twitter.length > 0) {
-	var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-	$("#topContacts").append(formattedTwitter);
-	$("#footerContacts").append(formattedTwitter);
-}
-
 if(bio.contacts.location.length > 0) {
 	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
 	$("#topContacts").append(formattedLocation);
 	$("#footerContacts").append(formattedLocation);
 }
 
-//* * * * * Picture and Welcome Msg * * * * *//
+/* * * * * Picture and Welcome Msg * * * * */
 
 var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
     $("#header").prepend(formattedBioPic);
@@ -150,7 +144,7 @@ var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
 var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
     $("#header").append(formattedWelcomeMsg);
 
-//* * * * * Skills at a glance * * * * *//
+/* * * * * Skills at a glance * * * * */
 
 if(bio.skills.length > 0) {
 	$("#header").append(HTMLskillsStart);
@@ -164,23 +158,113 @@ if(bio.skills.length > 0) {
 	$("#skills").append(formattedSkill);
 }
 
-//* * * * * Work Experience * * * * *//
+/* * * * * Work Experience * * * * */
 
 for (job in work.jobs) {
 	$("#workExperience").append(HTMLworkStart);
+
 	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
 	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
 	var formattedEmployerTitle = formattedEmployer + formattedTitle;
-
 	$(".work-entry:last").append(formattedEmployerTitle);
 
-	var formattedDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+	var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
     $(".work-entry:last").append(formattedDates);
+
+    var formattedworkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+    $(".work-entry:last").append(formattedworkLocation);
 
     var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
     $(".work-entry:last").append(formattedDescription);
-
 };
+
+/* * * * * Projects * * * * */
+
+for (project in projects.projects) {
+	$(".small-block-grid-3").append(HTMLprojectStart);
+
+	var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+	$(".project-entry:last").append(formattedProjectTitle);
+
+	var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+	$(".project-entry:last").append(formattedProjectDates);
+
+	var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+	$(".project-entry:last").append(formattedProjectDescription);
+
+	var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[project].image);
+	$(".project-entry:last").append(formattedProjectImage);
+}
+
+/* * * * * Education * * * * */
+
+for (school in education.schools) {
+	$("#education").append(HTMLschoolStart);
+
+	var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+	var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+	$(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+	
+	var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+	$(".education-entry:last").append(formattedSchoolLocation);
+
+	var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+	$(".education-entry:last").append(formattedSchoolDates);
+
+	var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+	$(".education-entry:last").append(formattedSchoolMajor);
+
+}
+
+$("#education").append(HTMLonlineClasses);
+
+for (onlineCourse in education.onlineCourses) {
+
+	$("#education").append(HTMLonlineStart);
+
+	var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[onlineCourse].title);
+	var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[onlineCourse].school);
+	$(".online-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+
+
+	var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[onlineCourse].dates);
+	$(".online-entry:last").append(formattedOnlineDates);
+
+	var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[onlineCourse].url);
+	$(".online-entry:last").append(formattedOnlineURL);
+
+}
+
+/* * * * * Internationalized Name * * * * */
+
+function inName() {
+	var name = bio.name.trim().split(" ");
+	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+	name[1] = name[1].toUpperCase();
+	return name[0]+" "+name[1];
+};
+
+$("#header").append(internationalizeButton);
+
+/* * * * * Map * * * * */
+
+$("#mapDiv").append(googleMap);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
